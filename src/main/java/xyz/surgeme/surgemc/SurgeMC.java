80c17fb79b818b8aa5621a3557c65884fccbd368
TharmsyXD <133595791+TharmsyXD@users.noreply.github.com>
2024-01-02 23:46:49 +0000

    there

---
+++ src/main/java/xyz/surgeme/surgemc/SurgeMC.java
@@ -0,0 +1,17 @@
+package xyz.surgeme.surgemc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurgeMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
