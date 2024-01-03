5f990e05375c7158d17a41903472384f953b9d69
TharmsyXD <133595791+TharmsyXD@users.noreply.github.com>
2024-01-03 00:14:30 +0000

    Ligma hosting

---
+++ src/main/java/xyz/surgeme/surgemc/SurgeMC.java
@@ -0,0 +1,17 @@
+package xyz.surgeme.surgemc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurgeMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // ligma hjasditj
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
